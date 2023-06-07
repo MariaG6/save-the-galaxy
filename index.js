7c91c0f36aad6f5f631ae3313f180cd96b657c8f
Maria Garcia <mariagarciamontes06@outlook.com>
2023-06-07 21:44:24 +0200

    Add a player and start to move it

--- index.js
+++ index.js
@@ -3,32 +3,76 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 // Set a background
+document.getElementById("start-btn").onclick = () => startGame();
 const battleBackground = new Image();
-battleBackground.src = '/images/battle-background.jpg';
-// battleBackground.style.width = canvas.width;
-// battleBackground.style.height = canvas.height;
+battleBackground.src = "/images/battle-background.jpg";
 
 function setBackground() {
   ctx.drawImage(battleBackground, 0, 0, canvas.width, canvas.height);
-  console.log(battleBackground);
 }
-setBackground();
 
 // Create a Player
 class Player {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
+  constructor() {
+    this.x = 20;
+    this.y = 400;
     const img = document.createElement("img");
     img.addEventListener("load", () => {
       this.img = img;
     });
     img.src = "/images/player-stop.png";
   }
+
+  draw() {
+    ctx.drawImage(this.img, this.x, this.y, 80, 80);
+  }
+
+  moveRight() {
+    this.x += 20;
+  }
+
+  moveLeft(){
+    this.x -= 20;
+  }
+
+  moveUp(){
+    this.y -=20;
+  }
+
+  moveDown(){
+    this.y +=20;
+  }
+}
+
+const luke = new Player();
+
+//Add movement to the player
+document.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "ArrowRight":
+      luke.moveRight();
+      break;
+    case 'ArrowLeft':
+      luke.moveLeft();
+      break;
+    case 'ArrowUp':
+      luke.moveUp();
+      break;
+    case 'ArrowDown':
+      luke.moveDown();
+      break;
+  }
+  updater();
+});
+
+// Start the game
+function startGame() {
+  setBackground();
+  luke.draw();
 }
 
 // Update the game
 function updater() {
   ctx.clearRect(0, 0, 500, 500);
-  setBackground();
+  startGame();
 }
